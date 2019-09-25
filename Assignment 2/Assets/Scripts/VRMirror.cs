a3a507c4f5864a7729360eb2d25bbf9386fab360
Kelsey <kelseyfink@me.com>
2019-09-25 09:57:52 -0600

    Mirror attempt

---
+++ Assignment 2/Assets/Scripts/VRMirror.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VRMirror : MonoBehaviour {
+
+    bool isMirror = false;
+    bool isMatch = false;
+    public GameObject cube;
+    float rotationDiff = 0;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown("m"))
+        {
+            if(isMirror == false)
+            {
+                isMirror = true;
+                isMatch = false;
+            }
+            else
+            {
+                isMatch = true;
+                isMirror = false;
+            }
+        }
+        if(isMirror)
+        {
+            Vector3 currentRotation = transform.rotation.eulerAngles;
+            rotationDiff = 180 - currentRotation.y;
+            Vector3 mirrorRotation = currentRotation;
+            mirrorRotation = new Vector3(currentRotation.x * -1, currentRotation.y + rotationDiff, currentRotation.z);
+            cube.transform.rotation = Quaternion.Euler(currentRotation);
+            Debug.Log("mirror");
+        }
+        if(isMatch)
+        {
+            Debug.Log("match");
+        }
+    }
+}
