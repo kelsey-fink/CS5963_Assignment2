ef82f2a00d8d38ab855c035103b01e3795d2cd3b
Kelsey <kelseyfink@me.com>
2019-09-24 23:25:10 -0600

    Completed Camera Flipper

---
+++ Assignment 2/Assets/Scripts/CameraFlipper.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFlipper : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown("f"))
+        {
+            Vector3 currentRotation = transform.rotation.eulerAngles;
+            currentRotation = new Vector3(currentRotation.x, currentRotation.y + 180, currentRotation.z);
+            transform.rotation = Quaternion.Euler(currentRotation);
+        }
+    }
+}
