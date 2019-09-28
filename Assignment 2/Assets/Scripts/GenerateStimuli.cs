5662c239c6468f588a0574eba08d633f787b5811
Leo <u0986505@utah.edu>
2019-09-27 19:30:12 -0600

    Rotation tracking and depth seem to be kinda working

---
+++ Assignment 2/Assets/Scripts/GenerateStimuli.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenerateStimuli : MonoBehaviour {
+    public GameObject redSphere;
+    private MeshRenderer redMesh;
+    public GameObject blueLeftSphere;
+    private MeshRenderer blueLeftMesh;
+    public GameObject blueRightSphere;
+    private MeshRenderer blueRightMesh;
+
+    private float startBlueLeftDist;
+    private float startBlueRightDist;
+    private float startRedDist;
+
+    private bool enabled = true;
+    // Use this for initialization
+    void Start () {
+        redMesh = redSphere.GetComponent<MeshRenderer>();
+        blueLeftMesh = blueLeftSphere.GetComponent<MeshRenderer>();
+        blueRightMesh = blueRightSphere.GetComponent<MeshRenderer>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        startRedDist = Vector3.Distance(transform.position, redSphere.transform.position);
+        startBlueLeftDist = Vector3.Distance(transform.position, blueLeftSphere.transform.position);
+        startBlueRightDist = Vector3.Distance(transform.position, blueRightSphere.transform.position);
+        Vector3 redSize = redSphere.transform.localScale;
+        //Debug.Log("red size: " + redSize);
+
+        blueLeftSphere.transform.localScale = redSize * (startBlueLeftDist / startRedDist);
+        //Debug.Log("blue size left: " + (float)blueLeftSphere.transform.localScale.x + ", " + (float)blueLeftSphere.transform.localScale.y + ", " + (float)blueLeftSphere.transform.localScale.z);
+
+        blueRightSphere.transform.localScale = redSize * (startBlueRightDist / startRedDist);
+       // Debug.Log("blue size right: " + (float)blueRightSphere.transform.localScale.x + ", " + (float)blueRightSphere.transform.localScale.y + ", " + (float)blueRightSphere.transform.localScale.z);
+
+
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            if(enabled)
+            {
+                redMesh.enabled = false;
+
+                float wait = 2.0f;
+                float time_waited = 0.0f;
+
+                while (time_waited < wait)
+                {
+                    time_waited += Time.deltaTime;
+                    Debug.Log(time_waited);
+                }
+
+                blueLeftMesh.enabled = false;
+                blueRightMesh.enabled = false;
+                enabled = false;
+            }
+            else
+            {
+                enabled = true;
+                redMesh.enabled = true;
+
+                float wait = 2.0f;
+                float time_waited = 0.0f;
+
+                while (time_waited < wait)
+                {
+                    time_waited += Time.deltaTime;
+                }
+
+                blueLeftMesh.enabled = true;
+                blueRightMesh.enabled = true;
+            }
+        }
+    }
+}
--- Assignment 2/Assets/Scripts/ToggleTracking.cs
+++ Assignment 2/Assets/Scripts/ToggleTracking.cs
@@ -16,6 +16,30 @@ public class ToggleTracking : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+
+        if (rotation)
+        {
+            Debug.Log("Rotation on");
+            Vector3 currentRot = transform.rotation.eulerAngles;
+            Vector3 parentRot = parent.transform.rotation.eulerAngles;
+            //currentRot = new Vector3(currentRot.x - originalRot.x, currentRot.y - originalRot.y, currentRot.z - originalRot.z);
+
+            //Debug.Log("Before: \n");
+            //Debug.Log("Parent: " + parentRot);
+            //Debug.Log("Original: " + originalRot);
+            //Debug.Log("Current: " + currentRot);
+            parent.transform.RotateAround(originalPos, new Vector3(1, 0, 0), (currentRot.x - originalRot.x));
+            parent.transform.RotateAround(originalPos, new Vector3(0, 1, 0), -(currentRot.y - originalRot.y));
+            parent.transform.RotateAround(originalPos, new Vector3(0, 0, 1), (currentRot.z - originalRot.z));
+
+            parentRot = parent.transform.rotation.eulerAngles;
+            //Debug.Log("After: \n");
+            //Debug.Log("Parent: " + parentRot);
+            //Quaternion.Euler(currentRot));
+
+            originalRot = transform.rotation.eulerAngles;
+            originalPos = transform.position;
+        }
         if (position)
         {
             Debug.Log("Position on");
@@ -23,26 +47,34 @@ public class ToggleTracking : MonoBehaviour {
             Vector3 parentPos = parent.transform.position;
             parent.transform.position = new Vector3(parentPos.x - currentPos.x + originalPos.x, parentPos.y - currentPos.y + originalPos.y, parentPos.z - currentPos.z + originalPos.z);
         }
-        if(rotation)
-        {
-            Debug.Log("Rotation on");
-            Vector3 currentRot = transform.rotation.eulerAngles;
-            Vector3 parentRot = parent.transform.rotation.eulerAngles;
-            currentRot = new Vector3(currentRot.x, currentRot.y, currentRot.z);
-            parent.transform.rotation = Quaternion.Euler(currentRot);
-        }
 
         if (Input.GetKeyDown(KeyCode.R))
         {
-            originalRot = parent.transform.rotation.eulerAngles;
-            position = false;
-            rotation = true;
+            if(rotation)
+            {
+                rotation = false;
+            }
+            else
+            {
+                position = false;
+                rotation = true;
+            }
+            originalPos = transform.position;
+            originalRot = transform.rotation.eulerAngles;
+
         }
         if (Input.GetKeyDown(KeyCode.P))
         {
+            if(position)
+            {
+                position = false;
+            }
+            else
+            {
+                rotation = false;
+                position = true;
+            }
             originalPos = transform.position;
-            rotation = false;
-            position = true;
         }
-	}
+    }
 }
